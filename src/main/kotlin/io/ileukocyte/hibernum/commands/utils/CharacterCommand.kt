da2f55680406f575626670ce06b9c383f9afd3cb
ileukocyte <alex.ilakeful@gmail.com>
2021-08-01 12:41:52 +0300

    slash command impl

--- src/main/kotlin/io/ileukocyte/hibernum/commands/utils/CharacterCommand.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/commands/utils/CharacterCommand.kt
@@ -1,51 +1,65 @@
 package io.ileukocyte.hibernum.commands.utils
 
 import io.ileukocyte.hibernum.Immutable
+import io.ileukocyte.hibernum.builders.buildEmbed
+import io.ileukocyte.hibernum.commands.Command
 import io.ileukocyte.hibernum.commands.CommandException
 import io.ileukocyte.hibernum.commands.NoArgumentsException
-import io.ileukocyte.hibernum.commands.TextOnlyCommand
 import io.ileukocyte.hibernum.extensions.capitalizeAll
 import io.ileukocyte.hibernum.extensions.remove
-import io.ileukocyte.hibernum.extensions.sendEmbed
 
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
-class CharacterCommand : TextOnlyCommand {
+class CharacterCommand : Command {
     override val name = "char"
     override val description = "Sends some information about the provided character(s) (including built-in emojis)"
     override val aliases = setOf("character")
+    override val options = setOf(OptionData(OptionType.STRING, "input", "The characters provided", true))
 
     override suspend fun invoke(event: GuildMessageReceivedEvent, args: String?) {
         val input = args?.remove(" ")
             ?.run { takeIf { it.length <= 20 } ?: throw CommandException("The amount of characters must not exceed 20!") }
             ?: throw NoArgumentsException
 
-        event.channel.sendEmbed {
-            color = Immutable.SUCCESS
+        event.channel.sendMessageEmbeds(charEmbed(input)).queue()
+    }
 
-            for (codePoint in input.codePoints().distinct()) {
-                val chars = Character.toChars(codePoint)
-                var mainHex = Integer.toHexString(codePoint).uppercase()
+    override suspend fun invoke(event: SlashCommandEvent) {
+        val input = event.getOption("input")?.asString?.remove(" ")
+            ?.run { takeIf { it.length <= 20 } ?: throw CommandException("The amount of characters must not exceed 20!") }
+            ?: return
 
-                while (mainHex.length < 4)
-                    mainHex = "0$mainHex"
+        event.replyEmbeds(charEmbed(input)).queue()
+    }
 
-                val hex = if (chars.size > 1) {
-                    "`\\u$mainHex` (`" + chars.joinToString("") {
-                        var hex = Integer.toHexString(it.code).uppercase()
+    private fun charEmbed(input: String) = buildEmbed {
+        color = Immutable.SUCCESS
 
-                        while (hex.length < 4)
-                            hex = "0$hex"
+        for (codePoint in input.codePoints().distinct()) {
+            val chars = Character.toChars(codePoint)
+            var mainHex = Integer.toHexString(codePoint).uppercase()
 
-                        "\\u$hex"
-                    } + "`)"
-                } else "`\\u$mainHex`"
+            while (mainHex.length < 4)
+                mainHex = "0$mainHex"
 
-                field {
-                    title = Character.getName(codePoint)?.capitalizeAll() ?: "Unknown Character"
-                    description = buildString { appendLine("${String(chars)} — $hex") }
-                }
+            val hex = if (chars.size > 1) {
+                "`\\u$mainHex` (`" + chars.joinToString("") {
+                    var hex = Integer.toHexString(it.code).uppercase()
+
+                    while (hex.length < 4)
+                        hex = "0$hex"
+
+                    "\\u$hex"
+                } + "`)"
+            } else "`\\u$mainHex`"
+
+            field {
+                title = Character.getName(codePoint)?.capitalizeAll() ?: "Unknown Character"
+                description = buildString { appendLine("${String(chars)} — $hex") }
             }
-        }.queue()
+        }
     }
 }
