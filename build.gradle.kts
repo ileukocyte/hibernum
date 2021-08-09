4d951dd2f89da66c91f5ff6759d64d3e4c0daff4
ileukocyte <alex.ilakeful@gmail.com>
2021-08-09 15:46:08 +0300

    Version bump to 1.1.2-BETA1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 group = "io.ileukocyte"
-version = Version(major = 1, minor = 1, patch = 1)
+version = Version(major = 1, minor = 1, patch = 2, unstable = 1, stability = Version.Stability.Beta)
 
 repositories {
     mavenCentral()
