897b88e3cc2feb9d56e56822411e246609e23ab3
ileukocyte <alex.ilakeful@gmail.com>
2021-08-09 15:03:36 +0300

    ChooseCommand slash command implementation

--- src/main/kotlin/io/ileukocyte/hibernum/commands/utils/ChooseCommand.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/commands/utils/ChooseCommand.kt
@@ -1,16 +1,20 @@
 package io.ileukocyte.hibernum.commands.utils
 
 import io.ileukocyte.hibernum.Immutable
+import io.ileukocyte.hibernum.commands.Command
 import io.ileukocyte.hibernum.commands.NoArgumentsException
-import io.ileukocyte.hibernum.commands.TextOnlyCommand
+import io.ileukocyte.hibernum.extensions.replyEmbed
 import io.ileukocyte.hibernum.extensions.sendEmbed
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
-class ChooseCommand : TextOnlyCommand {
+class ChooseCommand : Command {
     override val name = "choose"
     override val description = "Chooses a random option among the provided ones split with the \"|\" character"
+    override val aliases = setOf("random")
     override val usages = setOf("option 1> | <option 2> | ... | <option n")
     override val options = setOf(
         OptionData(OptionType.STRING, "options", "The options provided (split with \"|\")", true)
@@ -23,6 +27,19 @@ class ChooseCommand : TextOnlyCommand {
             ?: throw NoArgumentsException
 
         event.channel.sendEmbed {
+            color = Immutable.SUCCESS
+            description = input.random()
+        }.queue()
+    }
+
+    override suspend fun invoke(event: SlashCommandEvent) {
+        val input = event.getOption("options")?.asString
+            ?.split(Regex("\\s?\\|\\s?"))
+            ?.filter { it.isNotEmpty() }
+            ?.takeUnless { it.isEmpty() }
+            ?: throw NoArgumentsException
+
+        event.replyEmbed {
             color = Immutable.SUCCESS
             description = input.random()
         }.queue()
