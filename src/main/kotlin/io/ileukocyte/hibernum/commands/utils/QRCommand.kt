071dc102e522adda9485af04a08e85968a28fcd4
ileukocyte <alex.ilakeful@gmail.com>
2021-09-05 04:57:27 +0300

    Finalized implementation

--- src/main/kotlin/io/ileukocyte/hibernum/commands/utils/QRCommand.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/commands/utils/QRCommand.kt
@@ -1,14 +1,24 @@
 package io.ileukocyte.hibernum.commands.utils
 
+import io.ileukocyte.hibernum.commands.Command
 import io.ileukocyte.hibernum.commands.NoArgumentsException
-import io.ileukocyte.hibernum.commands.TextOnlyCommand
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+
 import net.glxn.qrgen.core.image.ImageType
 import net.glxn.qrgen.javase.QRCode
 
-class QRCommand : TextOnlyCommand {
+class QRCommand : Command {
     override val name = "qr"
-    override val description = "N/A"
+    override val description = "Generates a QR code from the provided input"
+    override val usages = setOf("input")
+    override val options = setOf(
+        OptionData(OptionType.STRING, "input", "The provided input", true)
+    )
+    override val cooldown = 3L
 
     override suspend fun invoke(event: GuildMessageReceivedEvent, args: String?) {
         val qr = QRCode.from(args ?: throw NoArgumentsException)
@@ -17,5 +27,14 @@ class QRCommand : TextOnlyCommand {
             .stream()
 
         qr.use { event.channel.sendFile(it.toByteArray(), "qr.png").queue() }
+    }
+
+    override suspend fun invoke(event: SlashCommandEvent) {
+        val qr = QRCode.from(event.getOption("input")?.asString ?: return)
+            .withSize(768, 768)
+            .to(ImageType.PNG)
+            .stream()
+
+        qr.use { event.deferReply().addFile(it.toByteArray(), "qr.png").queue() }
     }
 }
