e3186573b282f9cc6c7a831d70959389a5770c4b
ileukocyte <alex.ilakeful@gmail.com>
2021-09-11 12:09:42 +0300

    PruneCommand options renamed

--- src/main/kotlin/io/ileukocyte/hibernum/commands/moderation/PruneCommand.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/commands/moderation/PruneCommand.kt
@@ -5,13 +5,14 @@ import io.ileukocyte.hibernum.commands.CommandCategory
 import io.ileukocyte.hibernum.commands.CommandException
 import io.ileukocyte.hibernum.commands.SlashOnlyCommand
 import io.ileukocyte.hibernum.extensions.*
+
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.interactions.commands.Command.Choice
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
+
 import org.apache.commons.validator.routines.UrlValidator
 
 class PruneCommand : SlashOnlyCommand {
@@ -19,20 +20,20 @@ class PruneCommand : SlashOnlyCommand {
     override val description = "Deletes messages by the amount and filters provided"
     override val category = CommandCategory.BETA
     override val options = setOf(
-        OptionData(OptionType.INTEGER, "count", "The amount of messages to delete (max 1000)", true),
+        OptionData(OptionType.INTEGER, "count", "The amount of messages to delete (up to 1000)", true),
         OptionData(OptionType.USER, "user", "The user whose messages are to delete"),
         OptionData(OptionType.STRING, "filter", "Type of messages to delete")
             .let { o ->
-                val options = setOf("embeds", "invites", "attachments", "bots", "links", "mentions")
+                val options = setOf("attachments", "bots", "embeds", "invites", "links", "mentions")
 
                 o.addChoices(options.map { Choice(it.capitalizeAll(), it) })
             },
-        OptionData(OptionType.STRING, "content-filter", "Type of message content check")
+        OptionData(OptionType.STRING, "text-filter", "Type of message content check")
             .addChoice("Contains", "contains")
             .addChoice("Does Not Contain", "not")
             .addChoice("Starts With", "startswith")
             .addChoice("Ends With", "endswith"),
-        OptionData(OptionType.STRING, "content", "The content to filter messages by")
+        OptionData(OptionType.STRING, "text", "The content to filter messages by")
     )
     override val cooldown = 3L
 
@@ -46,8 +47,8 @@ class PruneCommand : SlashOnlyCommand {
         if (Permission.MESSAGE_MANAGE !in member.permissions)
             throw CommandException("You do not have the required permission to manage messages!")
 
-        if (event.getOption("content-filter") !== null && event.getOption("content") === null)
-            throw CommandException("The `content` option must be initialized in case of the `content-filter` option being provided!")
+        if (event.getOption("text-filter") !== null && event.getOption("text") === null)
+            throw CommandException("The `text` option must be initialized in case of the `text-filter` option being provided!")
 
         val count = event.getOption("count")?.asLong?.toInt()?.takeIf { it in 1..1000 }
             ?: throw CommandException("The provided amount is out of the required range of 1 through 1,000!")
@@ -59,29 +60,29 @@ class PruneCommand : SlashOnlyCommand {
         val filtered = history.filter { message ->
             val filter = event.getOption("filter")?.let {
                 when (it.asString) {
-                    "embeds" -> message.embeds.isNotEmpty()
-                    "invites" -> message.invites.isNotEmpty()
                     "attachments" -> message.attachments.isNotEmpty()
                     "bots" -> message.author.isBot
+                    "embeds" -> message.embeds.isNotEmpty()
+                    "invites" -> message.invites.isNotEmpty()
                     "links" -> message.contentRaw.split(" ").any { s -> UrlValidator.getInstance().isValid(s) }
                     "mentions" -> message.mentionedRoles.isNotEmpty() || message.mentionedUsers.isNotEmpty()
                     else -> true
                 }
             } ?: true
 
-            val contentFilter = event.getOption("content-filter")?.let {
-                val content = event.getOption("content")?.asString ?: return
+            val textFilter = event.getOption("text-filter")?.let {
+                val text = event.getOption("text")?.asString ?: return
 
                 when (it.asString) {
-                    "contains" -> content in message.contentRaw
-                    "not" -> content !in message.contentRaw
-                    "startswith" -> message.contentRaw.startsWith(content)
-                    "endswith" -> message.contentRaw.endsWith(content)
+                    "contains" -> text in message.contentRaw
+                    "not" -> text !in message.contentRaw
+                    "startswith" -> message.contentRaw.startsWith(text)
+                    "endswith" -> message.contentRaw.endsWith(text)
                     else -> true
                 }
             } ?: true
 
-            message.author == (event.getOption("user")?.asUser ?: message.author) && filter && contentFilter
+            message.author == (event.getOption("user")?.asUser ?: message.author) && filter && textFilter
         }.take(count).takeUnless { it.isEmpty() } ?: run {
             deferred.editOriginalEmbeds(defaultEmbed("No messages to delete have been found!", EmbedType.FAILURE)).queue()
             return
@@ -91,7 +92,7 @@ class PruneCommand : SlashOnlyCommand {
             filtered.size,
             event.getOption("user")?.asUser,
             event.getOption("filter")?.asString,
-            event.getOption("content-filter")?.asString
+            event.getOption("text-filter")?.asString
         )
 
         event.channel.purgeMessages(filtered)
@@ -99,7 +100,7 @@ class PruneCommand : SlashOnlyCommand {
         deferred.editOriginalEmbeds(defaultEmbed(response, EmbedType.SUCCESS)).queue()
     }
 
-    fun getResponse(deletedMessages: Int, user: User?, filter: String?, contentFilter: String?) = buildString {
+    fun getResponse(deletedMessages: Int, user: User?, filter: String?, textFilter: String?) = buildString {
         append("Deleted $deletedMessages ")
 
         if (filter == "embeds") append("embed ")
@@ -108,16 +109,16 @@ class PruneCommand : SlashOnlyCommand {
 
         filter?.let {
             append(when (it) {
-                "embeds" -> ""
-                "invites" -> " containing invite links"
                 "attachments" -> " containing any attachments"
                 "bots" -> " sent by bots"
+                "embeds" -> ""
+                "invites" -> " containing invite links"
                 "links" -> " containing any URLs"
                 else -> " mentioning any users or any roles"
             })
         }
 
-        contentFilter?.let {
+        textFilter?.let {
             if (filter !== null && user !== null) append(",")
             else if (filter !== null) append(" and")
 
@@ -130,8 +131,8 @@ class PruneCommand : SlashOnlyCommand {
         }
 
         user?.let {
-            if (filter !== null && contentFilter !== null) append(", and")
-            else if (filter !== null || contentFilter !== null) append (" and")
+            if (filter !== null && textFilter !== null) append(", and")
+            else if (filter !== null || textFilter !== null) append (" and")
 
             append(" sent by ${it.asMention}")
         }
