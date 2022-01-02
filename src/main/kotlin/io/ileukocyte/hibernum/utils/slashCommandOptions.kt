aebdbb694d379da53c9616b9f675c8d37fe84f2c
ileukocyte <alex.ilakeful@gmail.com>
2022-01-02 06:39:29 +0200

    Slash commands options update checker fixed

--- src/main/kotlin/io/ileukocyte/hibernum/utils/slashCommandOptions.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/utils/slashCommandOptions.kt
@@ -4,19 +4,26 @@ import net.dv8tion.jda.api.interactions.commands.Command.Option
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
-internal fun Option.toOptionData() = OptionData(type, name, description, isRequired)
+ fun Option.toOptionData() = OptionData(type, name, description, isRequired)
     .let { if (type == OptionType.CHANNEL) it.setChannelTypes(channelTypes) else it }
     .let { if (type.canSupportChoices()) it.addChoices(choices) else it }
     .let {
-        if (type == OptionType.NUMBER || type == OptionType.INTEGER) {
-            minValue?.let { min -> it.setMinValue(min) }
-            maxValue?.let { min -> it.setMaxValue(min) }
+        when (type) {
+            OptionType.NUMBER -> {
+                minValue?.toDouble()?.let { min -> it.setMinValue(min) }
+                maxValue?.toDouble()?.let { max -> it.setMaxValue(max) }
+            }
+            OptionType.INTEGER -> {
+                minValue?.toLong()?.let { min -> it.setMinValue(min) }
+                maxValue?.toLong()?.let { max -> it.setMaxValue(max) }
+            }
+            else -> {}
         }
 
         it
     }
 
-internal fun List<OptionData>.isEqualTo(another: List<OptionData>): Boolean {
+ fun List<OptionData>.isEqualTo(another: List<OptionData>): Boolean {
     fun OptionData.isEqualTo(anotherData: OptionData) =
         name == anotherData.name
                 && description == anotherData.description
@@ -37,16 +44,4 @@ internal fun List<OptionData>.isEqualTo(another: List<OptionData>): Boolean {
     }
 
     return true
-}
-
-private fun OptionData.setMinValue(min: Number) = when (min) {
-    is Double -> setMinValue(min)
-    is Long -> setMinValue(min)
-    else -> throw UnsupportedOperationException()
-}
-
-private fun OptionData.setMaxValue(max: Number) = when (max) {
-    is Double -> setMaxValue(max)
-    is Long -> setMaxValue(max)
-    else -> throw UnsupportedOperationException()
 }
