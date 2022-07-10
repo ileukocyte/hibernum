c92a2bf9873216a701b5412ad727c2f3e9683649
ileukocyte <alex.ilakeful@gmail.com>
2022-07-10 02:13:50 +0200

    SayCommand attachment option

--- src/main/kotlin/io/ileukocyte/hibernum/commands/utility/SayCommand.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/commands/utility/SayCommand.kt
@@ -3,9 +3,11 @@ package io.ileukocyte.hibernum.commands.utility
 import io.ileukocyte.hibernum.Immutable
 import io.ileukocyte.hibernum.commands.Command
 import io.ileukocyte.hibernum.commands.NoArgumentsException
+import io.ileukocyte.hibernum.extensions.await
 import io.ileukocyte.hibernum.extensions.limitTo
 import io.ileukocyte.hibernum.extensions.sendEmbed
 
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.interactions.commands.OptionType
@@ -17,20 +19,23 @@ class SayCommand : Command {
     override val aliases = setOf("announce")
     override val usages = setOf(setOf("message"), setOf("image file"))
     override val options = setOf(
-        OptionData(OptionType.STRING, "message", "The message to send on behalf of the bot", true))
+        OptionData(OptionType.STRING, "text", "The text to send on behalf of the bot"),
+        OptionData(OptionType.ATTACHMENT, "attachment", "The attachment to send on behalf of the bot"),
+    )
 
     override suspend fun invoke(event: MessageReceivedEvent, args: String?) {
-        if (args === null && event.message.attachments.none { it.isImage })
+        if (args === null && event.message.attachments.none { it.isImage }) {
             throw NoArgumentsException
+        }
 
         val (imageName, imageStream) = event.message.attachments.firstOrNull { it.isImage }
-            .let { it?.fileName to it?.proxy?.download() }
+            .let { it?.fileName to it?.proxy?.download()?.await() }
 
         event.message.delete().queue()
 
         val restAction = event.channel.sendEmbed {
             color = Immutable.SUCCESS
-            description = args?.limitTo(4000)
+            description = args?.limitTo(MessageEmbed.DESCRIPTION_MAX_LENGTH)
             image = imageStream?.let { "attachment://$imageName" }
 
             author {
@@ -39,21 +44,33 @@ class SayCommand : Command {
             }
         }
 
-        imageStream?.thenAccept { restAction.addFile(it, imageName.orEmpty()).queue() }
-            ?: restAction.queue()
+        imageStream?.let { restAction.addFile(it, imageName.orEmpty()).queue() } ?: restAction.queue()
     }
 
     override suspend fun invoke(event: SlashCommandInteractionEvent) {
+        val text = event.getOption("text")?.asString
+        val attachment = event.getOption("attachment")
+            ?.asAttachment
+            ?.takeIf { it.isImage }
+            ?.let { it.fileName to it.proxy.download().await() }
+
+        if (text === null && attachment === null) {
+            throw NoArgumentsException
+        }
+
         event.deferReply().queue { it.deleteOriginal().queue({}) {} }
 
-        event.channel.sendEmbed {
+        val restAction = event.channel.sendEmbed {
             color = Immutable.SUCCESS
-            description = event.getOption("message")?.asString?.limitTo(4000) ?: return
+            description = text?.limitTo(MessageEmbed.DESCRIPTION_MAX_LENGTH)
+            image = attachment?.first?.let { "attachment://$it" }
 
             author {
                 name = event.user.asTag
                 iconUrl = event.user.effectiveAvatarUrl
             }
-        }.queue()
+        }
+
+        attachment?.second?.let { restAction.addFile(it, attachment.first).queue() } ?: restAction.queue()
     }
 }
