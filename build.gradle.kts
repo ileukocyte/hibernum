8e8c632e2688fd32d825d1b7f02d2d1c051cf460
ileukocyte <alex.ilakeful@gmail.com>
2022-07-10 17:39:30 +0200

    Version bumped to 2.4-BETA3

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "io.ileukocyte"
-version = Version(major = 2, minor = 4, unstable = 2, stability = Version.Stability.Beta)
+version = Version(major = 2, minor = 4, unstable = 3, stability = Version.Stability.Beta)
 
 repositories {
     mavenCentral()
