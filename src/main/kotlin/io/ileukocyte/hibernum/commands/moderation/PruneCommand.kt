8bbb2d0c143e26492efc348b4fbb2f5d93666826
ileukocyte <alex.ilakeful@gmail.com>
2022-07-13 05:06:08 +0200

    A lot more flexibility for &prune

--- src/main/kotlin/io/ileukocyte/hibernum/commands/moderation/PruneCommand.kt
+++ src/main/kotlin/io/ileukocyte/hibernum/commands/moderation/PruneCommand.kt
@@ -24,19 +24,47 @@ class PruneCommand : SlashOnlyCommand {
         OptionData(OptionType.INTEGER, "count", "The amount of messages to delete (up to 1000)", true)
             .setMinValue(1)
             .setMaxValue(1000),
-        OptionData(OptionType.USER, "user", "The user whose messages are to delete"),
+        OptionData(OptionType.USER, "user", "The user whose messages are to (not) delete"),
         OptionData(OptionType.STRING, "filter", "Type of messages to delete")
             .let { o ->
-                val options = setOf("attachments", "bots", "embeds", "invites", "links", "mentions", "unpinned")
+                val options = sortedSetOf(
+                    "all attachments",
+                    "all embeds",
+                    "bots",
+                    "bot embeds",
+                    "gifs",
+                    "images",
+                    "invites",
+                    "links",
+                    "mentions",
+                    "no attachments",
+                    "stickers",
+                    "unpinned",
+                    "users aside from",
+                    "videos",
+                )
 
-                o.addChoices(options.map { Choice(it.capitalizeAll(), it) })
+                val choices = options.map {
+                    val label = if (it == "gifs") {
+                        "GIFs"
+                    } else {
+                        it.capitalizeAll()
+                    }
+
+                    Choice(label, it.replace(" ", "-"))
+                }
+
+                o.addChoices(choices)
             },
         OptionData(OptionType.STRING, "text-filter", "Type of message content check")
             .addChoice("Contains", "contains")
             .addChoice("Does Not Contain", "not")
             .addChoice("Starts With", "startswith")
-            .addChoice("Ends With", "endswith"),
+            .addChoice("Ends With", "endswith")
+            .addChoice("Equals To", "equals")
+            .addChoice("Does Not Equal To", "doesntequal"),
         OptionData(OptionType.STRING, "text", "The content to filter messages by"),
+        OptionData(OptionType.BOOLEAN, "text-case-sensitive", "Case sensitivity for text filtering (default is true)"),
         OptionData(OptionType.MENTIONABLE, "mention", "The mentions that messages to delete contain"),
     )
     override val cooldown = 5L
@@ -44,11 +72,23 @@ class PruneCommand : SlashOnlyCommand {
     override val botPermissions = setOf(Permission.MESSAGE_MANAGE)
 
     override suspend fun invoke(event: SlashCommandInteractionEvent) {
-        if (event.getOption("text-filter") !== null && event.getOption("text") === null)
-            throw CommandException("The \"text\" option must be initialized in case of the \"text-filter\" option being provided!")
+        val filterOption = event.getOption("filter")?.asString
 
-        if (event.getOption("mention") !== null && event.getOption("filter")?.asString != "mentions")
+        if (filterOption == "users-aside-from" && event.getOption("user")?.asUser === null) {
+            throw CommandException("The \"user\" option must be initialized in case of the \"filter\" option set to \"Users Aside From\"!")
+        }
+
+        if (event.getOption("text-filter") !== null && event.getOption("text") === null) {
+            throw CommandException("The \"text\" option must be initialized in case of the \"text-filter\" option being provided!")
+        }
+
+        if (event.getOption("mention") !== null && filterOption != "mentions") {
             throw CommandException("The \"filter\" option must be set to \"mentions\" in case of the \"mention\" option being provided!")
+        }
+
+        if (filterOption == "bot-embeds" && event.getOption("user")?.asUser?.isBot == false) {
+            throw CommandException("No messages to delete have been found!")
+        }
 
         val count = event.getOption("count")?.asLong?.toInt() ?: return
 
@@ -62,13 +102,18 @@ class PruneCommand : SlashOnlyCommand {
                 return@forEachAsync false
             }
 
-            val filter = event.getOption("filter")?.let {
+            val filter = filterOption?.let {
                 val mention = event.getOption("mention")?.asMentionable
 
-                when (it.asString) {
-                    "attachments" -> message.attachments.isNotEmpty()
+                when (it) {
+                    "all-attachments" -> message.attachments.isNotEmpty()
+                    "all-embeds" -> message.embeds.isNotEmpty()
                     "bots" -> message.author.isBot
-                    "embeds" -> message.embeds.isNotEmpty()
+                    "bot-embeds" -> message.author.isBot && message.embeds.isNotEmpty()
+                    "gifs" -> message.attachments.any { a -> a.fileExtension == "gif" }
+                            || message.contentRaw.split("\\s+".toRegex())
+                                .any { a -> UrlValidator.getInstance().isValid(a) && (".gif" in a || "-gif-" in a) }
+                    "images" -> message.attachments.any { a -> a.isImage }
                     "invites" -> message.invites.isNotEmpty()
                     "links" -> message.contentRaw.split(" ").any { s -> UrlValidator.getInstance().isValid(s) }
                     "mentions" -> {
@@ -82,26 +127,55 @@ class PruneCommand : SlashOnlyCommand {
                             users.isNotEmpty() || roles.isNotEmpty()
                         }
                     }
+                    "no-attachments" -> message.attachments.isEmpty()
+                    "stickers" -> message.stickers.isNotEmpty()
                     "unpinned" -> !message.isPinned
+                    "users-aside-from" -> true
+                    "videos" -> message.attachments.any { a -> a.isVideo }
                     else -> true
                 }
             } ?: true
 
             val textFilter = event.getOption("text-filter")?.let {
-                val text = event.getOption("text")!!.asString
+                val isCaseSensitive = event.getOption("text-case-sensitive")?.asBoolean
+                    ?: true
+
+                val text = event.getOption("text")!!.asString.let { t ->
+                    if (isCaseSensitive) {
+                        t
+                    } else {
+                        t.lowercase()
+                    }
+                }
+
+                val content = message.contentRaw.let { c ->
+                    if (isCaseSensitive) {
+                        c
+                    } else {
+                        c.lowercase()
+                    }
+                }
 
                 when (it.asString) {
-                    "contains" -> text in message.contentRaw
-                    "not" -> text !in message.contentRaw
-                    "startswith" -> message.contentRaw.startsWith(text)
-                    "endswith" -> message.contentRaw.endsWith(text)
+                    "contains" -> text in content
+                    "not" -> text !in content
+                    "startswith" -> content.startsWith(text)
+                    "endswith" -> content.endsWith(text)
+                    "equals" -> content == text
+                    "doesntequal" -> content != text
                     else -> true
                 }
             } ?: true
 
-            val predicate = message.author == (event.getOption("user")?.asUser ?: message.author)
-                    && filter
-                    && textFilter
+            val userFilter = event.getOption("user")?.asUser?.let {
+                if (filterOption == "users-aside-from") {
+                    message.author != it
+                } else {
+                    message.author == it
+                }
+            } ?: true
+
+            val predicate = userFilter && filter && textFilter
 
             if (predicate) {
                 counter++
@@ -144,45 +218,62 @@ class PruneCommand : SlashOnlyCommand {
     ) = buildString {
         append("Deleted $deletedMessages ")
 
-        if (filter == "embeds") append("embed ")
+        if (filter?.contains("embeds") == true) {
+            append("embed ")
+        }
 
         append("message".singularOrPlural(deletedMessages))
 
         filter?.let {
             append(when (it) {
-                "attachments" -> " containing any attachments"
-                "bots" -> " sent by bots"
-                "embeds" -> ""
+                "all-attachments" -> " containing any attachments"
+                "bots", "bot-embeds" -> " sent by bots" +
+                        user?.takeIf { u -> u.isBot }?.asMention?.let { u -> " ($u)" }.orEmpty()
+                "gifs" -> " containing any GIFs"
+                "images" -> " containing any images"
                 "invites" -> " containing invite links"
                 "links" -> " containing any links"
+                "mentions" -> " mentioning ${mention?.asMention ?: "any users or any roles"}"
+                "no-attachments" -> " containing no attachments"
+                "stickers" -> " containing any stickers"
                 "unpinned" -> " that ${if (deletedMessages == 1) "is" else "are"} not pinned"
-                else -> " mentioning ${mention?.asMention ?: "any users or any roles"}"
+                "videos" -> " containing any videos"
+                else -> ""
             })
         }
 
         textFilter?.let {
-            if (filter !== null) {
-                append(if (user !== null) "," else " and")
+            if (filter?.takeUnless { f -> f == "users-aside-from" || f == "all-embeds" } !== null) {
+                append(if (user !== null && filter != "bot-embeds") "," else " and")
             }
 
             append(when (it) {
                 "contains" -> " containing the provided text"
                 "not" -> " not containing the provided text"
                 "startswith" -> " starting with the provided text"
+                "equals" -> " with the content equal to the provided text"
+                "doesntequal" -> " with the content not equal to the provided text"
                 else -> " ending with the provided text"
             })
         }
 
-        user?.let {
-            if (filter !== null || textFilter !== null) {
-                if (filter !== null && textFilter !== null) {
+        user?.takeUnless { filter == "bot-embeds" }?.let {
+            if (filter?.takeUnless { f -> f == "users-aside-from" } !== null || textFilter !== null) {
+                if (filter?.takeUnless { f -> f == "users-aside-from" || f == "all-embeds" } !== null
+                    && textFilter !== null) {
                     append(",")
                 }
 
-                append (" and")
+                if (filter == "all-embeds") {
+                    if (textFilter !== null) {
+                        append(" and")
+                    }
+                } else {
+                    append(" and")
+                }
             }
 
-            append(" sent by ${it.asMention}")
+            append(" sent ${"not ".takeIf { filter == "users-aside-from" }.orEmpty()}by ${it.asMention}")
         }
 
         append("!")
